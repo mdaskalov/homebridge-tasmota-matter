a9c3356425d28296dd6e12cd8a17b5aa05a9176c
Milko Daskalov <mdaskalov@outlook.com>
2026-06-13 15:04:15 +0200

    added lights with color control

--- src/tasmotaTypes.ts
+++ src/tasmotaTypes.ts
@@ -99,7 +99,6 @@ export const DEVICE_TYPES: Record<string, TasmotaDeviceDefinition> = {
       },
       levelControl: {
         update: { path: 'Dimmer' },
-        moveToLevel: { cmd: 'Dimmer {value}' },
         moveToLevelWithOnOff: { cmd: 'Dimmer {value}' },
       },
     },
@@ -124,8 +123,80 @@ export const DEVICE_TYPES: Record<string, TasmotaDeviceDefinition> = {
       },
       levelControl: {
         update: { path: 'Channel{idx}' },
-        moveToLevel: { cmd: 'Channel{idx} {value}' },
         moveToLevelWithOnOff: { cmd: 'Channel{idx} {value}' },
+      },
+    },
+  },
+  LIGHTBULB_B_CT: {
+    deviceType: 'ColorTemperatureLight',
+    clusters: {
+      onOff: {
+        onOff: false,
+      },
+      levelControl: {
+        currentLevel: 254,
+        minLevel: 1,
+        maxLevel: 254,
+      },
+      colorControl: {
+        colorMode: 2,
+        colorTemperatureMireds: 250,
+        colorTempPhysicalMinMireds: 147,
+        colorTempPhysicalMaxMireds: 454,
+        coupleColorTempToLevelMinMireds: 147,
+      },
+    },
+    handlers: {
+      onOff: {
+        update: { path: 'POWER{idx}' },
+        on: { cmd: 'POWER{idx} ON' },
+        off: { cmd: 'POWER{idx} OFF' },
+      },
+      levelControl: {
+        update: { path: 'Dimmer' },
+        moveToLevelWithOnOff: { cmd: 'Dimmer {value}' },
+      },
+      colorControl: {
+        update: { path: 'CT' },
+        moveToColorTemperatureLogic: { cmd: 'CT {value}', res: { path: 'CT' } },
+        stopAllColorMovement: { cmd: 'CT: 0' },
+      },
+    },
+  },
+  LIGHTBULB_B_HS: {
+    deviceType: 'ExtendedColorLight',
+    clusters: {
+      onOff: {
+        onOff: false,
+      },
+      levelControl: {
+        currentLevel: 254,
+        minLevel: 1,
+        maxLevel: 254,
+      },
+      colorControl: {
+        colorMode: 0,
+        currentHue: 0,
+        currentSaturation: 254,
+        colorTempPhysicalMinMireds: 147,
+        colorTempPhysicalMaxMireds: 454,
+        colorTemperatureMireds: 250,
+        coupleColorTempToLevelMinMireds: 147,
+      },
+    },
+    handlers: {
+      onOff: {
+        update: { path: 'POWER{idx}' },
+        on: { cmd: 'POWER{idx} ON' },
+        off: { cmd: 'POWER{idx} OFF' },
+      },
+      levelControl: {
+        update: { path: 'Channel{idx}' },
+        moveToLevelWithOnOff: { cmd: 'Channel{idx} {value}' },
+      },
+      colorControl: {
+        update: { path: 'HSBColor' },
+        moveToColorTemperatureLogic: { cmd: 'HSBColor', res: { path: 'HSBColor' } },
       },
     },
   },
